e0f464cfc8af6dc5feb02a8194546a209d331001
Narnia112 <alkinoosreed@gmail.com>
2025-08-28 21:27:56 +0300

    STILL TOO MUCH CONFETTI

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 function makeConfetti(){
-    confetti({particleCount: 4, ticks: 100, angle:  60, spread: 45, shapes: ['circle', 'square', 'square'], colors: ["03045e","0077b6","00b4d8","90e0ef"], scalar: 1.25, origin: { x: 0.15, y: 0.75 } });
-    confetti({particleCount: 4, ticks: 100, angle: 120, spread: 45, shapes: ['circle', 'square', 'square'], colors: ["03045e","0077b6","00b4d8","90e0ef"], scalar: 1.25, origin: { x: 0.85, y: 0.75 } });
+    confetti({particleCount: 3, ticks: 50, angle:  60, spread: 45, shapes: ['circle', 'square', 'square'], colors: ["0077b6","00b4d8","90e0ef"], scalar: 1.25, origin: { x: 0, y: 0.75 } });
+    confetti({particleCount: 3, ticks: 50, angle: 120, spread: 45, shapes: ['circle', 'square', 'square'], colors: ["0077b6","00b4d8","90e0ef"], scalar: 1.25, origin: { x: 1, y: 0.75 } });
     requestAnimationFrame(makeConfetti);
 }
 makeConfetti();
